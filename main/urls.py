f0eb1e6775c34a62379a199ca75923bbda09e7c9
Aaron <aaron.kwek@ui.ac.id>
2023-10-04 15:38:42 +0700

    selesai mengerjakan tutorial

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
-from main.views import register 
-from main.views import login_user
-from main.views import logout_user
-from main.views import edit_product
-from main.views import delete_product
+from main.views import *
 app_name = 'main'
 
 urlpatterns = [
@@ -19,4 +14,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'), # sesuaikan dengan nama fungsi yang dibuat
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
 ]
